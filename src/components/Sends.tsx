8b308f582363e06c3fd953f5de94073b05a471c8
stevebarakat <stevebarakat@gmail.com>
2023-05-25 10:52:27 -0400

    sends persisting

--- src/components/Sends.tsx
+++ src/components/Sends.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Destination } from "tone";
 import type { Channel } from "tone";
 
@@ -7,6 +8,12 @@ type Props = {
 };
 
 function Sends({ trackIndex, channels }: Props) {
+  const currentTrackString = localStorage.getItem("currentTracks");
+  const currentTracks = currentTrackString && JSON.parse(currentTrackString);
+
+  const [activeBusses, setActiveBusses] = useState(
+    currentTracks[trackIndex].activeBusses ?? [false, false, false, false]
+  );
   return (
     <div className="bus-btn">
       <input
@@ -16,11 +23,21 @@ function Sends({ trackIndex, channels }: Props) {
           if (e.currentTarget.checked) {
             channels[trackIndex].send("reverb1");
             channels[trackIndex].send("delay1");
+            activeBusses[0] = true;
+            setActiveBusses([...activeBusses]);
           } else {
             channels[trackIndex].disconnect();
             channels[trackIndex].connect(Destination);
+            activeBusses[0] = false;
+            setActiveBusses([...activeBusses]);
           }
+          currentTracks[0].activeBusses = [...activeBusses];
+          localStorage.setItem(
+            "currentTracks",
+            JSON.stringify([...currentTracks])
+          );
         }}
+        checked={activeBusses[0]}
       />
       <label htmlFor={`bus1${trackIndex}`}>1</label>
       <input
@@ -28,13 +45,23 @@ function Sends({ trackIndex, channels }: Props) {
         type="checkbox"
         onChange={(e: React.FormEvent<HTMLInputElement>): void => {
           if (e.currentTarget.checked) {
-            channels[trackIndex].send("reverb2");
-            channels[trackIndex].send("delay2");
+            channels[1].send("reverb2");
+            channels[1].send("delay2");
+            activeBusses[1] = true;
+            setActiveBusses([...activeBusses]);
           } else {
-            channels[trackIndex].disconnect();
-            channels[trackIndex].connect(Destination);
+            channels[1].disconnect();
+            channels[1].connect(Destination);
+            activeBusses[1] = false;
+            setActiveBusses([...activeBusses]);
           }
+          currentTracks[1].activeBusses = [...activeBusses];
+          localStorage.setItem(
+            "currentTracks",
+            JSON.stringify([...currentTracks])
+          );
         }}
+        checked={activeBusses[1]}
       />
       <label htmlFor={`bus2${trackIndex}`}>2</label>
     </div>
--- src/utils/getSong.ts
+++ src/utils/getSong.ts
@@ -56,6 +56,7 @@ export function getSong(defaultSong: Song) {
     mute: false,
     solo: false,
     fx: ["nofx", "nofx"],
+    activeBusses: [false, false],
     trackPanelActive: true,
     trackPanelPosition: { x: 0, y: 0 },
     trackPanelSize: { width: "325px", height: "auto" },
