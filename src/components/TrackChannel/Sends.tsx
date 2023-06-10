0ccc273ecdaa5f5abeafff66de67c82c0511a634
stevebarakat <stevebarakat@gmail.com>
2023-06-10 03:23:40 -0400

    sends

--- src/components/TrackChannel/Sends.tsx
+++ src/components/TrackChannel/Sends.tsx
@@ -17,54 +17,36 @@ function Sends({ trackIndex, channels, busChannels }: Props) {
   );
   return (
     <div className="bus-btn">
-      <input
-        id={`bus1${trackIndex}`}
-        type="checkbox"
-        onChange={(e: React.FormEvent<HTMLInputElement>): void => {
-          if (e.currentTarget.checked) {
-            channels[trackIndex].disconnect();
-            channels[trackIndex].connect(busChannels[0]);
-            sends[0] = true;
-            setSends([...sends]);
-          } else {
-            channels[trackIndex].disconnect();
-            channels[trackIndex].connect(Destination);
-            sends[0] = false;
-            setSends([...sends]);
-          }
-          currentTracks[trackIndex].sends = [...sends];
-          localStorage.setItem(
-            "currentTracks",
-            JSON.stringify([...currentTracks])
-          );
-        }}
-        checked={sends[0]}
-      />
-      <label htmlFor={`bus1${trackIndex}`}>1</label>
-      <input
-        id={`bus2${trackIndex}`}
-        type="checkbox"
-        onChange={(e: React.FormEvent<HTMLInputElement>): void => {
-          if (e.currentTarget.checked) {
-            channels[trackIndex].disconnect();
-            channels[trackIndex].connect(busChannels[1]);
-            sends[1] = true;
-            setSends([...sends]);
-          } else {
-            channels[trackIndex].disconnect();
-            channels[trackIndex].connect(Destination);
-            sends[1] = false;
-            setSends([...sends]);
-          }
-          currentTracks[trackIndex].sends = [...sends];
-          localStorage.setItem(
-            "currentTracks",
-            JSON.stringify([...currentTracks])
-          );
-        }}
-        checked={sends[1]}
-      />
-      <label htmlFor={`bus2${trackIndex}`}>2</label>
+      {busChannels.map((_, busIndex) => (
+        <>
+          <input
+            id={`bus${busIndex + 1}fx${trackIndex + 1}`}
+            type="checkbox"
+            onChange={(e: React.FormEvent<HTMLInputElement>): void => {
+              if (e.currentTarget.checked) {
+                channels[trackIndex].disconnect();
+                channels[trackIndex].connect(busChannels[0]);
+                sends[busIndex] = true;
+                setSends([...sends]);
+              } else {
+                channels[trackIndex].disconnect();
+                channels[trackIndex].connect(Destination);
+                sends[busIndex] = false;
+                setSends([...sends]);
+              }
+              currentTracks[trackIndex].sends = [...sends];
+              localStorage.setItem(
+                "currentTracks",
+                JSON.stringify([...currentTracks])
+              );
+            }}
+            checked={sends[busIndex]}
+          />
+          <label htmlFor={`bus${busIndex + 1}fx${trackIndex + 1}`}>
+            {busIndex + 1}
+          </label>
+        </>
+      ))}
     </div>
   );
 }
