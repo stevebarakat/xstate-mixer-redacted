435cdcda2aa0ec3a363433dd8e57f73c46348c24
stevebarakat <stevebarakat@gmail.com>
2023-06-10 06:10:00 -0400

    position and size

--- src/components/TrackChannel/TrackPanel.tsx
+++ src/components/TrackChannel/TrackPanel.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from "react";
 import CloseButton from "../Buttons/CloseButton";
 import { Rnd as TrackFxPanel } from "react-rnd";
+import { TrackSettings } from "../../types/global";
 
 type Props = {
   children: ReactNode;
@@ -10,35 +11,72 @@ type Props = {
 };
 
 function TrackPanel({ children, trackIndex, active, setActive }: Props) {
-  const [position, setPosition] = useState([
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-  ]);
-  const [size, setSize] = useState([
-    { width: "325px", height: "auto" },
-    { width: "325px", height: "auto" },
-    { width: "325px", height: "auto" },
-    { width: "325px", height: "auto" },
-  ]);
+  const currentTracksString = localStorage.getItem("currentTracks");
+  const currentTracks = currentTracksString && JSON.parse(currentTracksString);
+
+  const positions = currentTracks.map(
+    (currentTrack: TrackSettings) => currentTrack.trackPanelPosition
+  );
+  const [position, setPosition] = useState(
+    positions ?? [
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+    ]
+  );
+
+  const sizes = currentTracks.map(
+    (currentTrack: TrackSettings) => currentTrack.trackPanelSize
+  );
+  const [size, setSize] = useState(
+    sizes ?? [
+      { width: "325px", height: "auto" },
+      { width: "325px", height: "auto" },
+      { width: "325px", height: "auto" },
+      { width: "325px", height: "auto" },
+    ]
+  );
 
   return (
     <TrackFxPanel
       className="fx-panel"
       position={position[trackIndex]}
       onDragStop={(_, d) => {
+        const currentTracksString = localStorage.getItem("currentTracks");
+        const currentTracks =
+          currentTracksString && JSON.parse(currentTracksString);
+
         position[trackIndex] = { x: d.x, y: d.y };
         setPosition([...position]);
+
+        currentTracks[trackIndex].trackPanelPosition = { x: d.x, y: d.y };
+        localStorage.setItem(
+          "currentTracks",
+          JSON.stringify([...currentTracks])
+        );
       }}
       size={size[trackIndex]}
       minWidth="200px"
       onResizeStop={(_, __, ref) => {
+        const currentTracksString = localStorage.getItem("currentTracks");
+        const currentTracks =
+          currentTracksString && JSON.parse(currentTracksString);
+
         size[trackIndex] = {
           width: ref.style.width,
-          height: ref.style.height,
+          height: "auto",
         };
         setSize([...size]);
+
+        currentTracks[trackIndex].trackPanelSize = {
+          width: ref.style.width,
+          height: "auto",
+        };
+        localStorage.setItem(
+          "currentTracks",
+          JSON.stringify([...currentTracks])
+        );
       }}
       cancel="input"
     >
