a6612417c610e8d688d7ed2be8bf16f2ed026c2d
stevebarakat <stevebarakat@gmail.com>
2023-06-09 22:22:09 -0400

    useSetTrackFx

--- src/components/TrackChannel/Track.tsx
+++ src/components/TrackChannel/Track.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Reverb, FeedbackDelay, PitchShift, Gain } from "tone";
-import { Reverber, PitchShifter, Delay, Signal } from "./Fx";
+import useSetTrackFx from "../../hooks/useSetTrackFx";
 import ChannelButton from "../Buttons/ChannelButton";
 import { Pan, SoloMute, Sends, Fader, TrackPanel } from "./";
 import useTrackFx from "../../hooks/useTrackFx";
@@ -15,17 +15,19 @@ type Props = {
   busChannels: Gain[];
 };
 
-type Fx = {
-  1: JSX.Element;
-  2: JSX.Element;
-};
-
-type FxTypes = FeedbackDelay | Reverb | PitchShift | Gain;
-
 function TrackChannel({ track, trackIndex, channels, busChannels }: Props) {
   const { fx, reverb, delay, pitchShift, gain } = useTrackFx({
     channels,
     trackIndex,
+  });
+
+  const { saveTrackFx } = useSetTrackFx({
+    reverb,
+    delay,
+    pitchShift,
+    gain,
+    trackIndex,
+    fx,
   });
 
   const currentTracksString = localStorage.getItem("currentTracks");
@@ -33,67 +35,7 @@ function TrackChannel({ track, trackIndex, channels, busChannels }: Props) {
   const ct = currentTracks[trackIndex];
   const channel = channels[trackIndex];
 
-  const [trackFx, setTrackFx] = useState(() => {
-    return (
-      currentTracks.fxName ?? Array(currentTracks.length).fill(["nofx", "nofx"])
-    );
-  });
-
   const [active, setActive] = useState([true, true, true, true]);
-
-  function saveTrackFx(e: React.FormEvent<HTMLSelectElement>) {
-    const currentTracksString = localStorage.getItem("currentTracks");
-    const currentTracks =
-      currentTracksString && JSON.parse(currentTracksString);
-
-    const sid = e.currentTarget.id.at(-1) ?? 0;
-    const id = parseInt(sid.toString(), 10);
-    trackFx[trackIndex][id] = e.currentTarget.value;
-    setTrackFx([...trackFx]);
-
-    currentTracks[trackIndex].fxName[id] = e.currentTarget.value;
-    localStorage.setItem("currentTracks", JSON.stringify([...currentTracks]));
-
-    console.log("id", id);
-    switch (e.currentTarget.value) {
-      case "nofx":
-        fx.current[`${id + 1}` as unknown as keyof Fx] = (
-          <Signal gain={gain.current} />
-        );
-        break;
-
-      case "reverb":
-        fx.current[`${id + 1}` as unknown as keyof Fx] = (
-          <Reverber reverb={reverb.current} trackIndex={trackIndex} />
-        );
-        break;
-
-      case "delay":
-        fx.current[`${id + 1}` as unknown as keyof Fx] = (
-          <Delay delay={delay.current} trackIndex={trackIndex} />
-        );
-        break;
-
-      case "pitchShift":
-        fx.current[`${id + 1}` as unknown as keyof Fx] = (
-          <PitchShifter
-            pitchShift={pitchShift.current}
-            trackIndex={trackIndex}
-          />
-        );
-        break;
-
-      default:
-        break;
-    }
-
-    const fxProps = Object.values(fx.current).map((fx) => fx.props);
-    const fxNodes = fxProps.map((prop) => Object.values(prop)[0]);
-    channel.disconnect();
-    fxNodes.forEach((node: FxTypes) => {
-      channel.chain(node);
-    });
-  }
 
   const disabled = currentTracks[trackIndex].fxName.every(
     (item: string) => item === "nofx"
---
+++ src/hooks/useSetTrackFx.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { Reverb, FeedbackDelay, PitchShift, Gain } from "tone";
+import {
+  Reverber,
+  PitchShifter,
+  Delay,
+  Signal,
+} from "../components/TrackChannel/Fx";
+
+type Fx = {
+  1: JSX.Element;
+  2: JSX.Element;
+};
+
+type FxTypes = FeedbackDelay | Reverb | PitchShift | Gain;
+
+type Props = {
+  trackIndex: number;
+  fx: React.MutableRefObject<Fx>;
+  reverb: React.MutableRefObject<Reverb>;
+  delay: React.MutableRefObject<FeedbackDelay>;
+  pitchShift: React.MutableRefObject<PitchShift>;
+  gain: React.MutableRefObject<Gain>;
+};
+
+function useSetTrackFx({
+  fx,
+  trackIndex,
+  reverb,
+  delay,
+  pitchShift,
+  gain,
+}: Props) {
+  const currentTracksString = localStorage.getItem("currentTracks");
+  const currentTracks = currentTracksString && JSON.parse(currentTracksString);
+
+  const [trackFx, setTrackFx] = useState(() => {
+    return (
+      currentTracks.fxName ?? Array(currentTracks.length).fill(["nofx", "nofx"])
+    );
+  });
+
+  function saveTrackFx(e: React.FormEvent<HTMLSelectElement>) {
+    const currentTracksString = localStorage.getItem("currentTracks");
+    const currentTracks =
+      currentTracksString && JSON.parse(currentTracksString);
+
+    const sid = e.currentTarget.id.at(-1) ?? 0;
+    const id = parseInt(sid.toString(), 10);
+    trackFx[trackIndex][id] = e.currentTarget.value;
+    setTrackFx([...trackFx]);
+
+    currentTracks[trackIndex].fxName[id] = e.currentTarget.value;
+    localStorage.setItem("currentTracks", JSON.stringify([...currentTracks]));
+
+    switch (e.currentTarget.value) {
+      case "nofx":
+        fx.current[`${id + 1}` as unknown as keyof Fx] = (
+          <Signal gain={gain.current} />
+        );
+        break;
+
+      case "reverb":
+        fx.current[`${id + 1}` as unknown as keyof Fx] = (
+          <Reverber reverb={reverb.current} trackIndex={trackIndex} />
+        );
+        break;
+
+      case "delay":
+        fx.current[`${id + 1}` as unknown as keyof Fx] = (
+          <Delay delay={delay.current} trackIndex={trackIndex} />
+        );
+        break;
+
+      case "pitchShift":
+        fx.current[`${id + 1}` as unknown as keyof Fx] = (
+          <PitchShifter
+            pitchShift={pitchShift.current}
+            trackIndex={trackIndex}
+          />
+        );
+        break;
+
+      default:
+        break;
+    }
+  }
+  return { saveTrackFx };
+}
+
+export default useSetTrackFx;
