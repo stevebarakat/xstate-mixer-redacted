770424bb951ff0221c930395f6646f8b3e23b3f1
stevebarakat <stevebarakat@gmail.com>
2023-06-12 04:58:54 -0400

    busses hydrated

--- src/hooks/useBusFx.ts
+++ src/hooks/useBusFx.ts
@@ -4,15 +4,18 @@ import { Gain, ScaleExp } from "tone";
 function useBusFx() {
   const scaleExp = useRef<ScaleExp | null>(null);
   const busChannels = useRef<BusChannel[]>([null, null]);
+  const currentMixString = localStorage.getItem("currentMix");
+  const currentMix = currentMixString && JSON.parse(currentMixString);
+  const busVolumes = currentMix.busVolumes;
 
   useEffect(() => {
     scaleExp.current = new ScaleExp(0, 1, 0.5);
     busChannels.current[0]?.disconnect();
-    busChannels.current[0] = new Gain(0)
+    busChannels.current[0] = new Gain(busVolumes[0])
       .connect(scaleExp.current)
       .toDestination();
     busChannels.current[1]?.disconnect();
-    busChannels.current[1] = new Gain(0)
+    busChannels.current[1] = new Gain(busVolumes[1])
       .connect(scaleExp.current)
       .toDestination();
 
